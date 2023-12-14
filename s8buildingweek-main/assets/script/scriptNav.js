c79b5dbad387c00c4db005d4058feca34c2c1a61
Davide Buglione <148747588+DavideB1997@users.noreply.github.com>
2023-12-14 11:14:24 +0100

    "brrrrr"

---
+++ s8buildingweek-main/assets/script/scriptNav.js
@@ -0,0 +1,11 @@
+document.addEventListener("scroll", function () {
+	let header = document.getElementById("header");
+
+	let scroll = window.scrollY;
+
+	if (scroll > 190) {
+		header.style.backgroundColor = "black";
+	} else {
+		header.style.backgroundColor = "transparent";
+	}
+});
